477877c50d76aaefc1e5f06ffe4392de80b2895b
Obedine-Flore <f.obedine@alustudent.com>
2024-07-03 23:40:14 +0300

    Files edited

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
-"""
-Route module for the API
-"""
-from flask import Flask, render_template, request
-from os import getenv
-from flask_babel import Babel
+"""Basic Flask app that implements i18n and internationalization"""
 
+from flask import Flask, render_template, request
+from flask_babel import Babel
 
 app = Flask(__name__)
 
 
 class Config:
-    """ class config"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    """Config class for your application, it deals with babel mostly"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
@@ -23,22 +20,18 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """matching locale lang"""
-    local_lang = request.args.get('locale')
-    support_lang = app.config['LANGUAGES']
-    if local_lang in support_lang:
-        return local_lang
-    else:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """Get locale for your application"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
-def index():
-    """hello world"""
-    return render_template("4-index.html", message="Welcome to Holberton")
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home():
+    """Home page for your application"""
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run()
