28827439cc35b63bdcb509fa5d8a66f6699c1012
Obedine-Flore <f.obedine@alustudent.com>
2024-07-14 00:38:33 +0300

    try

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const result = [];
-    for (const item of array) {
-      result.push(appendString + item);
-    }
-  
-    return result;
+  const result = [];
+  for (const item of array) {
+    result.push(appendString + item);
   }
+  return result;
+}
