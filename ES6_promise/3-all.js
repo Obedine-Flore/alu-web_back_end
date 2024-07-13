3b3acb81f460efd113d0dc5f42db527bbbe867af
Obedine-Flore <f.obedine@alustudent.com>
2024-07-14 00:52:59 +0300

    try

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from './utils';
+import { createUser, uploadPhoto } from './utils';
 
-export default async function handleProfileSignup() {
-  try {
-    const body = await uploadPhoto();
-    const user = await createUser();
-    console.log(`${body.body} ${user.firstName} ${user.lastName}`);
-  } catch (error) {
-    console.log('Signup system offline');
-  }
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
+
+export default handleProfileSignup;
