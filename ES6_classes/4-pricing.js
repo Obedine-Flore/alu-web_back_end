8607502450e89dee1878c786804f5b2afd40d3b0
Obedine Flore <f.obedine@alustudent.com>
2024-07-23 07:23:20 -0700

    my commit

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-variables */
+import Currency from './3-currency';
+/* eslint-enable no-unused-variables */
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+
